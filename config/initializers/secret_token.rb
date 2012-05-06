d34827a47eb848bd0bf164fae8e1e1604bf0029e
Frank Luithle <frank.luithle@barco.com>
2012-05-07 00:44:10 +0200

    Run rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookie::Application.config.secret_token = '67d1e69c4bb3577cb74749b62b87d48b4f882fc9046da85c5baaed99f0474ae452647409f0cc7d3299179a0847ffe6bc81953fcfb779e6da32c9699ae75336ab'
