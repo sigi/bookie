2aae571492f8ec2707c9b8baec01fbe4bf80e03f
Frank Luithle <frank@luithle.net>
2014-06-04 00:48:36 +0200

    Fix specials access to attributes

--- app/models/specialbet.rb
+++ app/models/specialbet.rb
@@ -1,14 +1,9 @@
 class Specialbet < ActiveRecord::Base
 
-  attr_accessible :user, :special1_id, :special2, :special3_id, :special4_id,
-                  :special5_id, :special6_id, :special7
+  attr_accessible :user, :special1_id, :special2, :special3, :special4
 
   belongs_to :user
   belongs_to :special1, :foreign_key => 'special1_id', :class_name => 'Team'
-  belongs_to :special3, :foreign_key => 'special3_id', :class_name => 'Team'
-  belongs_to :special4, :foreign_key => 'special4_id', :class_name => 'Team'
-  belongs_to :special5, :foreign_key => 'special5_id', :class_name => 'Team'
-  belongs_to :special6, :foreign_key => 'special6_id', :class_name => 'Team'
 
   def open?
     first_match = Match.find( :first, :order => 'date asc' )
