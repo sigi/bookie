e5a6ef5d41f81f24491cd07a31e499758e90fb19
Frank 'Sigi' Luithle <frank@luithle.net>
2021-06-09 18:11:09 +0200

    Prepare DB for Device OmniAuth support

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_145508) do
+ActiveRecord::Schema.define(version: 2021_06_09_161042) do
 
   create_table "bets", force: :cascade do |t|
     t.integer "result1", limit: 2, default: -1, null: false
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 2021_06_09_145508) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
