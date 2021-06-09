56c61c238ca586bd30ee2bc4f421d51d909d4300
Frank 'Sigi' Luithle <frank@luithle.net>
2021-06-09 21:45:09 +0200

    Add real_name field to sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   #before_action :setup_scoreboard, :set_query_user
@@ -42,6 +43,10 @@ protected
   def parse_count( value, default = 4 )
     count = value.to_i
     if count < 1 then default else count end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:real_name])
   end
 
 end
