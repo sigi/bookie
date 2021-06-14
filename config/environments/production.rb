8b531e44e6f69f48012f6a2d1d2219bda56d6a46
Frank Luithle <frank@luithle.net>
2021-06-14 13:33:47 +0200

    Configure action mailer URL for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Required by Devise
+  config.action_mailer.default_url_options = { host: 'tippspiel.luithle.net' }
+
 end
